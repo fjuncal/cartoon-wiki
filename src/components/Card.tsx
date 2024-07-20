b8c7d9a446bf09be814bf0dcb6cead7cacdef100
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-20 01:38:45 -0300

    acertando css da pagina principal

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -41,7 +41,7 @@ export default function Card() {
                   role="listitem"
                   className="xl:w-1/3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5 px-2 "
                 >
-                  <div className="rounded overflow-hidden shadow-md bg-gray-200 ">
+                  <div className="rounded-3xl overflow-hidden shadow-md bg-gray-200 hover:bg-gray-600 hover:shadow-black hover:cursor-pointer">
                     <div className="absolute -mt-20 w-full flex justify-center ">
                       <div className="h-32 w-32">
                         <img
