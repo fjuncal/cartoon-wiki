af7c251c2836fdaa126f6b11ee4d1625a846b0f1
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-22 23:58:09 -0300

    Trocando a cor do loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="absolute right-1/2 bottom-1/2  transform translate-x-1/2 translate-y-1/2 ">
-      <div className="border-t-transparent border-solid animate-spin  rounded-full border-blue-400 border-8 h-64 w-64"></div>
+      <div className="border-t-transparent border-solid animate-spin  rounded-full border-gray-600 border-8 h-64 w-64"></div>
     </div>
   );
 }
